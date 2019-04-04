b07633f55448e5bc92e6a17109d3fe448f359add
Kevin McEwan <kevinmcewan@Kevins-MacBook-Pro.local>
2019-04-04 11:36:27 +0100

    changed function name

--- randomNumber.js
+++ randomNumber.js
@@ -1,11 +1,11 @@
-const randomNumber = function (min, max) {
+const randomInt = function (min, max) {
     const result = Math.floor(Math.random() * ((max + 1) - min) + min);
     return result;
 }
 
-const randomArrayResult = function(array){
-const result = randomNumber(0, array.length - 1);
-return array[result];
+const randomArrayResult = function (array) {
+    const result = randomInt(0, array.length - 1);
+    return array[result];
 }
 
 
